cddd40ad847fcdc7c1421097c9c8bea766fcf3dd
Alex Snaps <alex.snaps@gmail.com>
2018-05-23 23:14:06 -0400

    .get API

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,77 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+use std::collections::hash_map;
+use std::collections::hash_map::Entry;
+use std::ops::Fn;
+use std::sync::{Arc, RwLock};
+
+pub struct Cachers<K, V> {
+  data: RwLock<HashMap<K, Arc<V>>>,
+}
+
+impl<K, V> Cachers<K, V>
+  where K: std::cmp::Eq + std::hash::Hash,
+{
+  pub fn new() -> Cachers<K, V>
+  {
+    Cachers {
+      data: RwLock::new(HashMap::new()),
+    }
+  }
+
+  pub fn get<F>(&self, key: K, populating_fn: F) -> Option<Arc<V>>
+    where F: Fn(&K) -> Option<V>,
+  {
+    match self.data.write().unwrap().entry(key) {
+      Entry::Occupied(entry) => {
+        Some(entry.get().clone())
+      }
+      Entry::Vacant(entry) => {
+        insert_if_value(populating_fn(entry.key()), entry)
+      }
+    }
+  }
+}
+
+fn insert_if_value<K, V>(value: Option<V>, entry: hash_map::VacantEntry<K, Arc<V>>) -> Option<Arc<V>>
+{
+  match value {
+    Some(value) => {
+      let arc = Arc::new(value);
+      entry.insert(arc.clone());
+      Some(arc)
+    }
+    None => None
+  }
+}
 
 #[cfg(test)]
 mod tests {
+  use super::Cachers;
+
+  fn populate(key: &i32) -> Option<String> {
+    Some(key.to_string())
+  }
+
+  fn do_not_invoke(key: &i32) -> Option<String> {
+    assert_eq!("", "I shall not be invoked!");
+    Some(key.to_string())
+  }
+
   #[test]
   fn it_works() {
-    assert_eq!(21 * 2, 42);
+    let cache: Cachers<i32, String> = Cachers::new();
+    let our_key = 42;
+    {
+      let value = cache.get(our_key, populate);
+      assert_eq!(*value.unwrap(), "42");
+    }
+
+    {
+      let value = cache.get(our_key, do_not_invoke);
+      assert_eq!(*value.unwrap(), "42");
+    }
   }
 }
