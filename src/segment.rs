cb13450e500aab57d105585bc3bbcaeb4a82fd8a
Alex Snaps <alex.snaps@gmail.com>
2018-07-26 17:40:09 +0300

    Sort of working CacheEntry

--- src/segment.rs
+++ src/segment.rs
@@ -18,15 +18,25 @@ use std;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::ops::Fn;
+use std::sync::RwLock;
 
 pub struct Segment<K, V> {
   data: HashMap<K, CacheEntry<V>>,
   evictor: ClockEvictor<K>,
 }
 
-struct CacheEntry<V> {
+enum CacheEntry<V> {
+  Value(CacheValue<V>),
+  Lock(SoftLock<V>),
+}
+
+struct CacheValue<V> {
   value: V,
   index: usize,
+}
+
+struct SoftLock<V> {
+  value: RwLock<Option<CacheValue<V>>>,
 }
 
 impl<K, V> Segment<K, V>
@@ -43,10 +53,70 @@ where
 
   pub fn get(&self, key: &K) -> Option<V> {
     if let Some(cache_entry) = self.data.get(key) {
-      self.evictor.touch(cache_entry.index);
-      return Some(cache_entry.value.clone());
+      match cache_entry {
+        CacheEntry::Value(entry) => {
+          self.evictor.touch(entry.index);
+          return Some(entry.value.clone());
+        }
+        CacheEntry::Lock(_) => panic!("Fix me!"), // todo Handle lock
+      }
     }
     None
+  }
+
+  pub fn update<F>(&mut self, key: K, updating_fn: F) -> Option<V>
+  where
+    F: Fn(&K, Option<V>) -> Option<V>,
+  {
+    let mut remove_entry = false;
+    let (option, key_evicted) = match self.data.entry(key) {
+      Entry::Occupied(mut entry) => {
+        let cache_value = match entry.get_mut() {
+          CacheEntry::Value(entry) => entry,
+          CacheEntry::Lock(_) => panic!("Fix me!"), // todo Handle lock
+        };
+
+        match updating_fn(&key, Some(cache_value.value.clone())) {
+          Some(value) => {
+            cache_value.value = value;
+            self.evictor.touch(cache_value.index);
+            (Some(cache_value.value.clone()), None)
+          }
+          None => {
+            remove_entry = true;
+            (None, None)
+          }
+        }
+      }
+      Entry::Vacant(entry) => {
+        let (option, key_evicted) = match updating_fn(entry.key(), None) {
+          Some(value) => {
+            let (index, to_remove) = self.evictor.add(*entry.key());
+            let cache_entry = entry.insert(CacheEntry::Value(CacheValue { value, index }));
+            let entry = match cache_entry {
+              CacheEntry::Value(entry) => entry,
+              CacheEntry::Lock(_) => panic!("Fix me!"), // todo Handle lock
+            };
+            (Some(entry.value.clone()), to_remove)
+          }
+          None => (None, None),
+        };
+        (option, key_evicted)
+      }
+    };
+
+    if remove_entry {
+      self.data.remove(&key);
+    }
+
+    if key_evicted.is_some() {
+      self.data.remove(&key_evicted.unwrap());
+    }
+
+    match option {
+      Some(cache_entry) => Some(cache_entry.clone()),
+      None => None,
+    }
   }
 
   pub fn get_or_populate<F>(&mut self, key: K, populating_fn: F) -> Option<V>
@@ -54,17 +124,22 @@ where
     F: Fn(&K) -> Option<V>,
   {
     let (option, key_evicted) = match self.data.entry(key) {
-      Entry::Occupied(entry) => {
-        let cache_entry = entry.get();
-        self.evictor.touch(cache_entry.index);
-        (Some(cache_entry.value.clone()), None)
-      }
+      Entry::Occupied(entry) => match entry.get() {
+        CacheEntry::Value(entry) => {
+          self.evictor.touch(entry.index);
+          (Some(entry.value.clone()), None)
+        }
+        CacheEntry::Lock(_) => panic!("Fix me!"), // todo Handle lock
+      },
       Entry::Vacant(entry) => {
         let (option, to_remove) = match populating_fn(entry.key()) {
           Some(value) => {
             let (index, to_remove) = self.evictor.add(entry.key().clone());
-            let cache_entry = entry.insert(CacheEntry { value, index });
-            (Some(cache_entry.value.clone()), to_remove)
+            let cache_entry = entry.insert(CacheEntry::Value(CacheValue { value, index }));
+            match cache_entry {
+              CacheEntry::Value(entry) => (Some(entry.value.clone()), to_remove),
+              CacheEntry::Lock(_) => panic!("Fix me!"), // todo Handle lock
+            }
           }
           None => (None, None),
         };
@@ -77,46 +152,6 @@ where
     }
 
     option
-  }
-
-  pub fn update<F>(&mut self, key: K, updating_fn: F) -> Option<V>
-  where
-    F: Fn(&K, Option<V>) -> Option<V>,
-  {
-    let (option, key_evicted) = match self.data.entry(key) {
-      Entry::Occupied(mut entry) => match updating_fn(entry.key(), Some(entry.get().value.clone())) {
-        Some(value) => {
-          let cache_entry = entry.get_mut();
-          cache_entry.value = value;
-          self.evictor.touch(cache_entry.index);
-          (Some(cache_entry.value.clone()), None)
-        }
-        None => {
-          entry.remove();
-          (None, None)
-        }
-      },
-      Entry::Vacant(entry) => {
-        let (option, key_evicted) = match updating_fn(entry.key(), None) {
-          Some(value) => {
-            let (index, to_remove) = self.evictor.add(*entry.key());
-            let cache_entry = entry.insert(CacheEntry { value, index });
-            (Some(cache_entry.value.clone()), to_remove)
-          }
-          None => (None, None),
-        };
-        (option, key_evicted)
-      }
-    };
-
-    if key_evicted.is_some() {
-      self.data.remove(&key_evicted.unwrap());
-    }
-
-    match option {
-      Some(cache_entry) => Some(cache_entry.clone()),
-      None => None,
-    }
   }
 
   #[cfg(test)]
