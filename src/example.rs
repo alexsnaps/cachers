cc5a53bd5f3d8f117be78c2635cf7e58105beba8
Alex Snaps <alex@wcgw.dev>
2020-04-14 21:15:23 -0400

    Simpler I think

--- src/example.rs
+++ src/example.rs
@@ -3,7 +3,7 @@ extern crate lazy_static;
 extern crate cachers;
 
 use cachers::CacheThrough;
-use std::sync::{Arc, Barrier};
+use std::sync::Arc;
 use std::thread;
 
 lazy_static! {
@@ -11,15 +11,15 @@ lazy_static! {
 }
 
 pub fn main() {
-  let cache = CACHE.clone();
   let our_key = 42;
 
-  let barrier = Arc::new(Barrier::new(2));
-  let other_barrier = barrier.clone();
+  let thread_name = "main";
+
+  let value = CACHE.get(our_key, |key| Some(key.to_string())); // miss, so populating
+  println!("Value for '{}' on {} is {:?}", our_key, thread_name, *value.unwrap());
 
   let t = thread::spawn(move || {
-    other_barrier.wait(); // wait for main thread to populate
-    let value = cache.get(our_key, |_| unimplemented!()); // entry should be there!
+    let value = CACHE.get(our_key, |_| unimplemented!()); // entry should be there!
     println!(
       "Value for '{}' on {:?} was present: {:?}",
       our_key,
@@ -28,7 +28,7 @@ pub fn main() {
     );
     for offset in 1..3 {
       let new_key = our_key + offset;
-      let value = cache.get(new_key, |key| Some(key.to_string()));
+      let value = CACHE.get(new_key, |key| Some(key.to_string()));
       println!(
         "Added key '{}' on {:?}: {:?}",
         new_key,
@@ -38,12 +38,6 @@ pub fn main() {
     }
   });
 
-  let thread_name = thread::current().name().unwrap().to_string();
-
-  let value = CACHE.get(our_key, |key| Some(key.to_string())); // miss, so populating
-  println!("Value for '{}' on {} is {:?}", our_key, thread_name, *value.unwrap());
-
-  barrier.wait(); // let the other thread proceed …
   t.join().unwrap(); // … and let it finish
 
   for offset in 1..3 {
